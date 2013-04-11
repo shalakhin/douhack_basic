3dcb75f5e19ade7792588765108520382cbbf3ef
Olexandr Shalakhin <olexandr@shalakhin.com>
2013-04-11 14:52:39 +0300

    update production.py

--- src/douhack_basic/douhack_basic/settings/production.py
+++ src/douhack_basic/douhack_basic/settings/production.py
@@ -1,1 +1,14 @@
 from ._base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'douhack_basic',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
