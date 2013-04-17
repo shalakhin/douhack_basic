3696bd08ac8686b8b50018028cad326b39203eb8
Olexandr Shalakhin <olexandr@shalakhin.com>
2013-04-17 16:51:03 +0300

    change list_display order for the participants admin list page

--- apps/registrations/admin.py
+++ apps/registrations/admin.py
@@ -5,8 +5,9 @@ from .models import Participant
 
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = [
-        'name', 'contacts','email', 'city', 'need_apartments', 'confirmed',
-        'need_railway_station','created', 'updated', 'internal_comments_shortly']
+        'internal_comments_shortly', 'name', 'contacts','email', 'city',
+        'need_apartments', 'confirmed', 'need_railway_station','created', 
+        'updated']
     list_filter = ('city', 'created', 'updated', 'confirmed',
         'need_apartments', 'need_railway_station')
     readonly_fields = ('confirmation_code', 'created', 'updated',)
