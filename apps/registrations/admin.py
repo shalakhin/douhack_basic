1bd5ba64954bb007c9e719513a9dfe52617a13cb
Olexandr Shalakhin <olexandr@shalakhin.com>
2013-04-17 16:39:14 +0300

    missed comma

--- apps/registrations/admin.py
+++ apps/registrations/admin.py
@@ -5,7 +5,7 @@ from .models import Participant
 
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = [
-        'name', 'contacts','email', 'city', 'need_apartments', 'confirmed'
+        'name', 'contacts','email', 'city', 'need_apartments', 'confirmed',
         'need_railway_station','created', 'updated', 'internal_comments']
     list_filter = ('city', 'created', 'updated', 'confirmed',
         'need_apartments', 'need_railway_station')
