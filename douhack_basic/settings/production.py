1772938fd86ca4b042a37d5db5921dd21a4bb5b8
Olexandr Shalakhin <olexandr@shalakhin.com>
2013-04-25 15:50:12 +0300

    add sentry to the project to track errors

--- douhack_basic/settings/production.py
+++ douhack_basic/settings/production.py
@@ -4,4 +4,8 @@ import dj_database_url
 DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django.raven_compat',
+)
+
 DEBUG = False
