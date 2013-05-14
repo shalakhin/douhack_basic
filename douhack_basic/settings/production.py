5c3fde5378c8d445ab3cfe367012b1e823613513
Olexandr Shalakhin <olexandr@shalakhin.com>
2013-05-15 02:15:42 +0300

    messages can be sent now

--- douhack_basic/settings/production.py
+++ douhack_basic/settings/production.py
@@ -1,11 +1,18 @@
+import os
 from ._base import *
-
 import dj_database_url
+
 DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 INSTALLED_APPS = INSTALLED_APPS + (
     'raven.contrib.django.raven_compat',
 )
+
+EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
+EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
+EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
+EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
+EMAIL_USE_TLS = True
 
 DEBUG = False
